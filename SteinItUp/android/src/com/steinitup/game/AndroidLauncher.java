8c33a5c8ea417b1df89c27fcce794da914177d62
EricaPereira6 <106919298+EricaPereira6@users.noreply.github.com>
2023-02-19 19:13:47 +0000

    Add files via upload

---
+++ SteinItUp/android/src/com/steinitup/game/AndroidLauncher.java
@@ -0,0 +1,21 @@
+package com.steinitup.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.steinitup.game.SteinItUpGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SteinItUpGame(), config);
+	}
+
+	@Override
+	public void onBackPressed(){
+
+	}
+}
